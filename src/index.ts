4b3239b6958443d434e4bff84cf1f9f73a695f4d
WellDoItToo <welldoittoo@gmail.com>
2024-07-24 00:31:13 +0300

    add to npm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'source-map-support/register';
 import { ENV } from '@/env';
 import { testFunction } from '@/app/test';
+export { testFunction } from '@/app/test';
+export * from '@/env';
 
 console.log(`Starting ${ENV.APP_NAME} in ${ENV.NODE_ENV} mode`);
 console.log(`func testFunction():`, testFunction());
