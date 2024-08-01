07c61a9342457e177c9470707733a4c218711f66
WellDoItToo <welldoittoo@gmail.com>
2024-08-01 15:45:44 +0300

    update eslint versions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,23 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
-    ecmaVersion: 2022,
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 2021,
     sourceType: 'module',
   },
   env: {
     node: true,
     es6: true,
-    jest: true,
   },
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'test/**/*.ts', 'scripts/**/*.ts', '*.json'],
+    },
+  ],
+  ignorePatterns: ['node_modules', 'dist', '.git', '.idea', '*.d.ts'],
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
-import path from 'path'
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -7,7 +7,7 @@ export default defineConfig({
     environment: 'node',
     include: ['test/**/*.test.ts'],
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
